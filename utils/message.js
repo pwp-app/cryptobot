31047ebcbd14e6637efe23947d19330ba11de31b
BackRunner <dev@backrunner.top>
2021-04-24 02:57:55 +0800

    fix: format output

--- utils/message.js
+++ utils/message.js
@@ -3,6 +3,14 @@ const COIN_TYPE = {
   futures: '合约',
 };
 
+const formatNumber = (num) => {
+  let formatted = `${num}`;
+  while (formatted.endsWith(0)) {
+    formatted = formatted.substr(0, formatted.length - 1);
+  }
+  return formatted;
+}
+
 const buildMessage = (coin, price) => {
   const output = [`【${COIN_TYPE[coin.type]}】${coin.name.toUpperCase()}${coin.type === 'spot' ? '/' : ''}USDT`];
   const highPrice = parseFloat(price.highPrice, 10);
@@ -18,8 +26,8 @@ const buildMessage = (coin, price) => {
   output.push(`24小时交易额: ${parseFloat(price.quoteVolume, 10)} USD`);
   output.push(`24小时高点: ${highPrice}`);
   output.push(`24小时低点: ${lowPrice}`);
-  output.push(`距离低点: ${lowDistance.toFixed(8)} ${lowDistancePercent.toFixed(2)}%`);
-  output.push(`距离高点: -${highDistance.toFixed(8)} - ${highDistancePercent.toFixed(2)}%`);
+  output.push(`距离低点: ${formatNumber(lowDistance.toFixed(8))} ${lowDistancePercent.toFixed(2)}%`);
+  output.push(`距离高点: -${formatNumber(highDistance.toFixed(8))} -${highDistancePercent.toFixed(2)}%`);
   return output.join('\n');
 };
 
